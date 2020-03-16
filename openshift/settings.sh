2f1476e22129c2b0581fd1c12a7eb866ec210a3e
Wade Barnes <wade.barnes@shaw.ca>
2020-03-16 11:43:40 -0700

    Fix custom library reference for backup pipeline.
    Signed-off-by: Wade Barnes <wade.barnes@shaw.ca>

--- openshift/settings.sh
+++ openshift/settings.sh
@@ -2,4 +2,4 @@ export PROJECT_NAMESPACE="nnfbch"
 export GIT_URI="https://github.com/WadeBarnes/a2a-trust-over-ip-configurations.git"
 export GIT_REF="master"
 
-export skip_git_overrides="agent-build.json controller-build.json database-build.json"
+export skip_git_overrides="agent-build.json controller-build.json database-build.json backup-build.json"
