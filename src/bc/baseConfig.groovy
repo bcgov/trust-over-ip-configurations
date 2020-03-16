bf8f5ffe2c367eb5d0ec8cdea7d404bce929b00d
Wade Barnes <wade.barnes@shaw.ca>
2020-03-16 09:03:38 -0700

    Add CI/CD framework code
    Signed-off-by: Wade Barnes <wade.barnes@shaw.ca>

---
+++ src/bc/baseConfig.groovy
@@ -0,0 +1,19 @@
+    
+package bc;
+
+class baseConfig {
+  // Wait timeout in minutes
+  public static final int WAIT_TIMEOUT = 10
+
+  // Deployment Environment TAGs
+  public static final String[] DEPLOYMENT_ENVIRONMENT_TAGS = ['dev', 'test', 'prod']
+
+  // The name of the project namespace(s).
+  public static final String  NAME_SPACE = 'nnfbch'
+
+  // Instance Prefix
+  public static final String  PREFIX = 'vc-authn-'
+
+  // Apps - Listed in the order they should be tagged
+  public static final String[] APPS = ['vc-authn-database', 'vc-authn-agent', 'vc-authn-controller', 'vc-authn-backup']
+}
