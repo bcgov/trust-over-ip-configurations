1b8e4fbc2150373c77e35dce9c9f17b14e24e511
Wade Barnes <wade.barnes@shaw.ca>
2020-03-17 09:23:49 -0700

    Prepare for transfer.
    Signed-off-by: Wade Barnes <wade.barnes@shaw.ca>

--- openshift/settings.sh
+++ openshift/settings.sh
@@ -1,5 +1,5 @@
 export PROJECT_NAMESPACE="nnfbch"
-export GIT_URI="https://github.com/WadeBarnes/a2a-trust-over-ip-configurations.git"
+export GIT_URI="https://github.com/bcgov/a2a-trust-over-ip-configurations.git"
 export GIT_REF="master"
 
 export skip_git_overrides="agent-build.json controller-build.json database-build.json backup-build.json"
