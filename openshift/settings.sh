c137b118170669d8191392b46d36553ac69bf2ce
Wade Barnes <wade.barnes@shaw.ca>
2020-04-21 12:43:48 -0700

    Add to skip_git_overrides list.
    Signed-off-by: Wade Barnes <wade.barnes@shaw.ca>

--- openshift/settings.sh
+++ openshift/settings.sh
@@ -2,4 +2,4 @@ export PROJECT_NAMESPACE="nnfbch"
 export GIT_URI="https://github.com/bcgov/a2a-trust-over-ip-configurations.git"
 export GIT_REF="master"
 
-export skip_git_overrides="agent-build.json controller-build.json database-build.json backup-build.json"
+export skip_git_overrides="agent-build.json controller-build.json database-build.json backup-build.json api-build.json db-build.json wallet-build.json issuer-web-base-build.json issuer-web-build.json"
