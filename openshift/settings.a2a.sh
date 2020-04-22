0f827c436fa9444b0673fe0ee536945bf2f27afc
Wade Barnes <wade.barnes@shaw.ca>
2020-04-21 18:53:33 -0700

    Fix A2A issuer profile template filtering.
    Signed-off-by: Wade Barnes <wade.barnes@shaw.ca>

--- openshift/settings.a2a.sh
+++ openshift/settings.a2a.sh
@@ -1,3 +1,4 @@
 # Description: Credential issuer profile
 export SKIP_PIPELINE_PROCESSING=1
 export include_templates="issuer-agent-build issuer-agent-deploy issuer-api-build issuer-api-deploy issuer-db-build issuer-db-deploy issuer-web-base-build issuer-web-build issuer-web-deploy issuer-wallet-build issuer-wallet-deploy"
+export ignore_templates=""
