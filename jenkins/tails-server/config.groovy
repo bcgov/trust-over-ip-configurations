2cfb38a99049954c3dd260ac879cdac2404b0b52
Wade Barnes <wade.barnes@shaw.ca>
2020-05-16 14:22:55 -0700

    Add tails-server build pipeline.
    Signed-off-by: Wade Barnes <wade.barnes@shaw.ca>

---
+++ jenkins/tails-server/config.groovy
@@ -0,0 +1,14 @@
+// Pipeline Configuration Properties
+// Import this file into the pipeline using 'load'.
+class config extends bc.baseConfig {
+  // Build configuration
+  public static final String  APP_NAME = "tails-server"
+  public static final String[] BUILDS = ["${this.APP_NAME}"]
+
+  // Override defaults
+  public static final String  PREFIX = ''
+  public static final String  SUFFIX = ''
+
+}
+
+return new config();
