798500b2df98a0b9825a46f2663271c73957e5cf
Wade Barnes <wade.barnes@shaw.ca>
2020-05-16 14:07:58 -0700

    Add the build configurations for the tails-server
    Signed-off-by: Wade Barnes <wade.barnes@shaw.ca>

--- openshift/settings.sh
+++ openshift/settings.sh
@@ -3,6 +3,6 @@ export PROJECT_NAMESPACE="nnfbch"
 export GIT_URI="https://github.com/bcgov/a2a-trust-over-ip-configurations.git"
 export GIT_REF="master"
 
-export skip_git_overrides="agent-build.json controller-build.json database-build.json backup-build.json api-build.json db-build.json wallet-build.json issuer-web-base-build.json issuer-web-build.json"
+export skip_git_overrides="agent-build.json controller-build.json database-build.json backup-build.json api-build.json db-build.json wallet-build.json issuer-web-base-build.json issuer-web-build.json tails-server-build.json"
 
-export ignore_templates="issuer-agent-deploy issuer-api-deploy issuer-db-deploy issuer-web-deploy issuer-wallet-deploy"
+export ignore_templates="issuer-agent-deploy issuer-api-deploy issuer-db-deploy issuer-web-deploy issuer-wallet-deploy tails-server-deploy"
