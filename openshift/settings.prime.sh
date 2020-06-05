1eb8ca5d8959b012625ca27c62f3a10309759ec3
Emiliano Suñé <emiliano.sune@gmail.com>
2020-06-05 12:53:21 -0700

    Add prime issuer openshift configurations
    Signed-off-by: Emiliano Suñé <emiliano.sune@gmail.com>

---
+++ openshift/settings.prime.sh
@@ -0,0 +1,4 @@
+# Description: Prime Health profile
+export SKIP_PIPELINE_PROCESSING=1
+export include_templates="issuer-agent-build issuer-agent-deploy issuer-wallet-build issuer-wallet-deploy"
+export ignore_templates=""
