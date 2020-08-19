496532dec49600aa1849d0624d4c8e9d06c85bc9
Emiliano Suñé <emiliano.sune@gmail.com>
2020-08-19 15:10:53 -0700

    UPdates to vc-authn-agent to start in read-only mode without a public DID
    Signed-off-by: Emiliano Suñé <emiliano.sune@gmail.com>

--- openshift/templates/agent/agent-deploy.overrides.sh
+++ openshift/templates/agent/agent-deploy.overrides.sh
@@ -13,14 +13,14 @@ if createOperation; then
   # Ask the user to supply the sensitive parameters ...
   readParameter "ADMIN_API_KEY - Please provide the key for the agent's Admin API.  If left blank, a 32 character long base64 encoded value will be randomly generated using openssl:" ADMIN_API_KEY $(generateKey 32) "false"
   readParameter "WALLET_KEY - Please provide the wallet encryption key for the environment.  If left blank, a 48 character long base64 encoded value will be randomly generated using openssl:" WALLET_KEY $(generateKey) "false"
-  readParameter "WALLET_SEED - Please provide the indy wallet seed for the environment.  If left blank, a seed will be randomly generated using openssl:" WALLET_SEED $(generateSeed) "false"
+  # readParameter "WALLET_SEED - Please provide the indy wallet seed for the environment.  If left blank, a seed will be randomly generated using openssl:" WALLET_SEED $(generateSeed) "false"
   readParameter "WALLET_DID - Please provide the indy wallet did for the environment.  The default is an empty string:" WALLET_DID "" "false"
 else
   # Secrets are removed from the configurations during update operations ...
-  printStatusMsg "Update operation detected ...\nSkipping the prompts for ADMIN_API_KEY, WALLET_KEY, WALLET_SEED, and WALLET_DID secrets ... \n"
+  printStatusMsg "Update operation detected ...\nSkipping the prompts for ADMIN_API_KEY, WALLET_KEY, and WALLET_DID secrets ... \n"
   writeParameter "ADMIN_API_KEY" "prompt_skipped" "false"
   writeParameter "WALLET_KEY" "prompt_skipped" "false"
-  writeParameter "WALLET_SEED" "prompt_skipped" "false"
+  # writeParameter "WALLET_SEED" "prompt_skipped" "false"
   writeParameter "WALLET_DID" "prompt_skipped" "false"
 fi
 
