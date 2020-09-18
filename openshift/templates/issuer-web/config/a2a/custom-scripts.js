7f7764eb08b057be29cab85f45b5c0eff553bb68
Emiliano Suñé <emiliano.sune@gmail.com>
2020-09-18 15:45:32 -0700

    Update schema definition and proof-request configuration for accredited lawyer
    Signed-off-by: Emiliano Suñé <emiliano.sune@gmail.com>

--- openshift/templates/issuer-web/config/a2a/custom-scripts.js
+++ openshift/templates/issuer-web/config/a2a/custom-scripts.js
@@ -1,7 +1,20 @@
 /* Include all your custom JS code in here, it will be available to the app instance */
 
-
-/* An array containing custom functions that will be automatically registered with
- * SurveyJS so that they can be used in triggers.
- */
-surveyFunctions = [];
+function updateStatus() {
+    var descriptionField = "Membership Status";
+    var codeField = "Membership Status Code";
+    var survey = this.survey;
+    var mappings = {
+      PRAC: "Practising",
+      RET: "Retired",
+      OTHER: "Other"
+    };
+  
+    survey.setValue(descriptionField, mappings[survey.getValue(codeField)]);
+  }
+  
+  /* An array containing custom functions that will be automatically registered with
+   * SurveyJS so that they can be used in triggers.
+   */
+  surveyFunctions = [updateStatus];
+  
