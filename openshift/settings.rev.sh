f124320384d5ef53df17eaa3c88ea8ee061d8050
Emiliano Suñé <emiliano.sune@gmail.com>
2020-10-28 15:09:14 -0700

    Add agent config for rev profile
    Signed-off-by: Emiliano Suñé <emiliano.sune@gmail.com>

--- openshift/settings.rev.sh
+++ openshift/settings.rev.sh
@@ -1,4 +1,4 @@
-# Description: vc-authn-tmp controller - required until revocation is sorted out
+# Description: vc-authn-tmp controller/agent - required until revocation is sorted out
 export SKIP_PIPELINE_PROCESSING=1
-export include_templates="controller-rev-build controller-rev-deploy"
+export include_templates="controller-rev-build controller-rev-deploy agent-rev-deploy"
 export ignore_templates=""
