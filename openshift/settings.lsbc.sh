4ed63a10187dadc964fe2ae6ab588a1f3b723598
Emiliano Suñé <emiliano.sune@gmail.com>
2021-02-26 11:42:07 -0800

    Reorganized profiles
    Signed-off-by: Emiliano Suñé <emiliano.sune@gmail.com>

--- openshift/settings.lsbc.sh
+++ openshift/settings.lsbc.sh
@@ -1,4 +1,4 @@
 # Description: Law Society of BC profile
 export SKIP_PIPELINE_PROCESSING=1
-export include_templates="issuer-agent-build issuer-agent-deploy issuer-wallet-build issuer-wallet-deploy"
+export include_templates="issuer-agent-deploy issuer-wallet-deploy"
 export ignore_templates=""
--- openshift/settings.prime.sh
+++ openshift/settings.prime.sh
@@ -1,4 +1,4 @@
 # Description: Prime Health profile
 export SKIP_PIPELINE_PROCESSING=1
-export include_templates="issuer-agent-build issuer-agent-deploy issuer-wallet-build issuer-wallet-deploy visual-verifier-build visual-verifier-deploy"
+export include_templates="issuer-agent-deploy issuer-wallet-deploy visual-verifier-deploy"
 export ignore_templates=""
