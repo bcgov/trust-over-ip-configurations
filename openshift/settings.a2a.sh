6d8857a2f82e16a1b96d5a06540b99ccdd34963a
wadeking98 <wkingnumber2@gmail.com>
2021-03-12 17:40:30 -0800

    updates to naming converntions
    Signed-off-by: wadeking98 <wkingnumber2@gmail.com>

--- openshift/settings.a2a.sh
+++ openshift/settings.a2a.sh
@@ -1,4 +1,4 @@
 # Description: Credential issuer profile
 export SKIP_PIPELINE_PROCESSING=1
-export include_templates="issuer-agent-deploy issuer-api-deploy issuer-db-deploy issuer-web-deploy issuer-wallet-deploy"
+export include_templates="issuer-agent-deploy issuer-kit-api-deploy issuer-db-deploy issuer-web-deploy issuer-wallet-deploy"
 export ignore_templates=""
