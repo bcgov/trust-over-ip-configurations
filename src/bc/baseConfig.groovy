5223f7c8c393e4e23cf1525b22ea5a0c60a4d351
wadeking98 <wkingnumber2@gmail.com>
2021-03-26 15:52:35 -0700

    updated tag names for pipeline
    Signed-off-by: wadeking98 <wkingnumber2@gmail.com>

--- src/bc/baseConfig.groovy
+++ src/bc/baseConfig.groovy
@@ -18,5 +18,5 @@ class baseConfig {
   public static final String  SUFFIX = ''
 
   // Apps - Listed in the order they should be tagged
-  public static final String[] APPS = ['tails-server', 'vc-authn-database', 'vc-authn-agent', 'vc-authn-controller', 'backup', 'issuer-agent', 'issuer-api', 'issuer-db', 'issuer-wallet', 'issuer-web']
+  public static final String[] APPS = ['tails-server', 'vc-authn-database', 'vc-authn-agent', 'vc-authn-controller', 'backup', 'issuer-kit-agent', 'issuer-kit-wallet']
 }
