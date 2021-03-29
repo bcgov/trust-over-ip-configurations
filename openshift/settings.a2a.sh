91627f7359ed9bb5bbabb4bf0fa52ed07cfdd2d7
wadeking98 <wkingnumber2@gmail.com>
2021-03-29 14:59:32 -0700

    changes to PR suffix patterns
    Signed-off-by: wadeking98 <wkingnumber2@gmail.com>

--- openshift/settings.a2a.sh
+++ openshift/settings.a2a.sh
@@ -1,5 +1,5 @@
 # Description: Credential issuer profile
 export PROJECT_NAMESPACE="a99fd4"
 export SKIP_PIPELINE_PROCESSING=1
-export include_templates="issuer-agent-deploy issuer-kit-api-deploy issuer-web-deploy"
+export include_templates="issuer-agent-deploy issuer-api-deploy issuer-web-deploy"
 export ignore_templates=""
