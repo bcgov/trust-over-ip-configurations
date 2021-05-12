aaf20b58b6f8eb38473171bec6af0b69284b6a03
Wade Barnes <wade@neoterictech.ca>
2021-05-12 13:19:51 -0700

    Initial iteration of the Public Health Gateway agent
    Signed-off-by: Wade Barnes <wade@neoterictech.ca>

---
+++ openshift/settings.phg.sh
@@ -0,0 +1,4 @@
+# Description: Public Health Gateway agent profile
+export SKIP_PIPELINE_PROCESSING=1
+export include_templates="issuer-agent-deploy issuer-wallet-deploy"
+export ignore_templates=""
