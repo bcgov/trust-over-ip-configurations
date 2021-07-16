5179716c886767399b57500158cb882358fcad2f
Emiliano Suñé <emiliano.sune@gmail.com>
2021-07-16 12:22:16 -0700

    Add admin frontend to a2a, add non-revocable schema support, update KNPs
    Signed-off-by: Emiliano Suñé <emiliano.sune@gmail.com>

--- openshift/settings.a2a.sh
+++ openshift/settings.a2a.sh
@@ -1,6 +1,6 @@
 # Description: Credential issuer profile
 export PROJECT_NAMESPACE="a99fd4"
 export SKIP_PIPELINE_PROCESSING=1
-# this assumes that the agent, api and web builds are available from Essential Serives Delivery
-export include_templates="issuer-agent-deploy issuer-api-deploy issuer-web-deploy"
+# this assumes that the agent, api, admin and web builds are available from Essential Serives Delivery
+export include_templates="issuer-agent-deploy issuer-api-deploy issuer-web-deploy issuer-admin-deploy"
 export ignore_templates=""
--- openshift/settings.sh
+++ openshift/settings.sh
@@ -3,6 +3,6 @@ export PROJECT_NAMESPACE="e79518"
 export GIT_URI="https://github.com/bcgov/a2a-trust-over-ip-configurations.git"
 export GIT_REF="master"
 
-export skip_git_overrides="agent-build.json controller-build.json database-build.json backup-build.json api-build.json db-build.json issuer-agent-build.json wallet-build.json issuer-web-base-build.json issuer-web-build.json tails-server-build.json visual-verifier-build.json"
+export skip_git_overrides="agent-build.yaml controller-build.yaml database-build.yaml backup-build.yaml api-build.yaml db-build.yaml issuer-agent-build.yaml wallet-build.yaml issuer-web-base-build.yaml issuer-web-build.yaml tails-server-build.yaml visual-verifier-build.yaml"
 
-export ignore_templates="issuer-agent-deploy issuer-api-deploy issuer-db-deploy issuer-web-deploy issuer-wallet-deploy tails-server-deploy visual-verifier-deploy"
+export ignore_templates="issuer-agent-deploy issuer-api-deploy issuer-db-deploy issuer-admin-deploy issuer-web-deploy issuer-wallet-deploy tails-server-deploy visual-verifier-deploy"
