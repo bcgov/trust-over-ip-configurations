929151f7da8c34592b49d4c15677911232143298
Wade Barnes <wade@neoterictech.ca>
2021-07-26 11:07:07 -0700

    Fix pipeline configurations
    Signed-off-by: Wade Barnes <wade@neoterictech.ca>

--- jenkins/tails-server/config.groovy
+++ jenkins/tails-server/config.groovy
@@ -4,7 +4,7 @@ class config extends bc.baseConfig {
   // Build configuration
   public static final String IMAGE_NAME = "tails-server"
   public static final String APP_NAME = "tails-server"
-  public static final String[] BUILDS = ["${this.APP_NAME}"]
+  public static final String[] BUILDS = ["${this.IMAGE_NAME}"]
 
   // Override defaults
   public static final String  PREFIX = ''
