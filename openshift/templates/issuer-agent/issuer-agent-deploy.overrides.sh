a56432972e22c084d4daa77cd81daf978c3710e8
Ian Costanzo <ian@anon-solutions.ca>
2022-05-09 13:58:48 -0700

    Move config to issuer-agent directory; add overrides script
    Signed-off-by: Ian Costanzo <ian@anon-solutions.ca>

--- openshift/templates/issuer-agent/issuer-agent-deploy.overrides.sh
+++ openshift/templates/issuer-agent/issuer-agent-deploy.overrides.sh
@@ -10,6 +10,33 @@ fi
 # Special deployment parameters needed for injecting a user supplied settings into the deployment configuration
 # ----------------------------------------------------------------------------------------------------------------
 
+OUTPUT_FORMAT=json
+
+# Generate application config map
+# - To include all of the files in the application instance's profile directory.
+# Injected by genDepls.sh
+# - APP_CONFIG_MAP_NAME
+# - PREFIX
+# - DEPLOYMENT_ENV_NAME
+
+# Combine the profile's default config files with its environment specific config files before generating the config map ...
+profileRoot=$( dirname "$0" )/config
+profileEnv=${profileRoot}/${DEPLOYMENT_ENV_NAME}
+profileTmp=$( dirname "$0" )/config/${PROFILE}/tmp
+mkdir -p ${profileTmp}
+cp -f ${profileRoot}/* ${profileTmp} 2>/dev/null
+cp -f ${profileEnv}/* ${profileTmp} 2>/dev/null
+
+# Generate the config map ...
+APPCONFIG_SOURCE_PATH=${profileTmp}
+APPCONFIG_OUTPUT_FILE=${APP_CONFIG_MAP_NAME}-configmap_DeploymentConfig.json
+printStatusMsg "Generating ConfigMap; ${APP_CONFIG_MAP_NAME} ..."
+generateConfigMap "${PREFIX}${APP_CONFIG_MAP_NAME}" "${APPCONFIG_SOURCE_PATH}" "${OUTPUT_FORMAT}" "${APPCONFIG_OUTPUT_FILE}"
+
+# Remove temporary configuration directory and files ....
+rm -rf ${profileTmp}
+unset SPECIALDEPLOYPARMS
+
 if createOperation; then
   # Ask the user to supply the sensitive parameters ...
   readParameter "ADMIN_API_KEY - Please provide the key for the agent's Admin API.  If left blank, a 32 character long base64 encoded value will be randomly generated using openssl:" ADMIN_API_KEY $(generateKey 32) "false"
