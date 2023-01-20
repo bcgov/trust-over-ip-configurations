bb1ed25c3f7aaf501c1e02130282df192079b628
Emiliano Suñé <emiliano.sune@gmail.com>
2023-01-20 15:26:23 -0800

    Fix controller override script
    Signed-off-by: Emiliano Suñé <emiliano.sune@gmail.com>

--- openshift/templates/orgbook-issuer-controller/orgbook-issuer-controller-deploy.overrides.sh
+++ openshift/templates/orgbook-issuer-controller/orgbook-issuer-controller-deploy.overrides.sh
@@ -5,6 +5,8 @@ else
   _red='\033[0;31m'; _yellow='\033[1;33m'; _nc='\033[0m'; echo -e \\n"${_red}overrides.inc could not be found on the path.${_nc}\n${_yellow}Please ensure the openshift-developer-tools are installed on and registered on your path.${_nc}\n${_yellow}https://github.com/BCDevOps/openshift-developer-tools${_nc}"; exit 1;
 fi
 
+OUTPUT_FORMAT=json
+
 # Generate application config map
 # - To include all of the files in the application instance's profile directory.
 # Injected by genDepls.sh
