ed2ea2a005be30c81e710b73c0d56c8993582c9a
Emiliano Suñé <emiliano.sune@gmail.com>
2023-03-07 17:39:42 -0800

    Make CONTROLLER_API_KEY mandatory
    Signed-off-by: Emiliano Suñé <emiliano.sune@gmail.com>

--- openshift/templates/orgbook-issuer-controller/orgbook-issuer-controller-deploy.overrides.sh
+++ openshift/templates/orgbook-issuer-controller/orgbook-issuer-controller-deploy.overrides.sh
@@ -41,12 +41,14 @@ unset SPECIALDEPLOYPARMS
 if createOperation; then
   readParameter "CR_AGENT_ADMIN_URL - Please provide the URL for the credential registry admin api.\nThe default is an empty string:" CR_AGENT_ADMIN_URL "" "false"
   readParameter "CR_ADMIN_API_KEY - Please provide the key for the credential registry admin api.\nThe default is an empty string:" CR_ADMIN_API_KEY "" "false"
+  readParameter "CONTROLLER_API_KEY - Please provide the value for the controller api-key. If left blank, a 32 character long base64 encoded value will be randomly generated using openssl:" CONTROLLER_API_KEY $(generateKey 32) "false"
 else
   # Secrets are removed from the configurations during update operations ...
-  printStatusMsg "Update operation detected ...\nSkipping the prompts for the CR_AGENT_ADMIN_URL, and CR_ADMIN_API_KEY secrets ...\n"
+  printStatusMsg "Update operation detected ...\nSkipping the prompts for the CR_AGENT_ADMIN_URL, CR_ADMIN_API_KEY and CONTROLLER_API_KEY secrets ...\n"
   # Prompted
   writeParameter "CR_AGENT_ADMIN_URL" "generation_skipped" "false"
   writeParameter "CR_ADMIN_API_KEY" "generation_skipped" "false"
+  writeParameter "CONTROLLER_API_KEY" "generation_skipped" "false"
 fi
 
 SPECIALDEPLOYPARMS="--param-file=${_overrideParamFile}"
