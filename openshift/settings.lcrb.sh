489c550a33ab0a80c40dca4d4686a1e4cae01e20
Emiliano Suñé <emiliano.sune@gmail.com>
2023-03-13 16:53:39 -0700

    Add LCRB agent configurations
    Signed-off-by: Emiliano Suñé <emiliano.sune@gmail.com>

--- openshift/settings.lcrb.sh
+++ openshift/settings.lcrb.sh
@@ -1,4 +1,4 @@
 # Description: BC Registries profile
 export SKIP_PIPELINE_PROCESSING=1
-export include_templates="issuer-agent-deploy issuer-wallet-deploy orgbook-issuer-controller-build orgbook-issuer-controller-deploy"
+export include_templates="orgbook-issuer-agent-deploy issuer-wallet-deploy orgbook-issuer-controller-deploy"
 export ignore_templates=""
