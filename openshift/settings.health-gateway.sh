177c5922a90a12f4096aa363e69db6b4f2829432
Emiliano Suñé <emiliano.sune@gmail.com>
2023-03-21 08:37:50 -0700

    Remove HG, Prime profiles
    Signed-off-by: Emiliano Suñé <emiliano.sune@gmail.com>

--- openshift/settings.health-gateway.sh
+++
@@ -1,4 +0,0 @@
-# Description: Health Gateway agent profile
-export SKIP_PIPELINE_PROCESSING=1
-export include_templates="issuer-agent-deploy issuer-wallet-deploy visual-verifier-deploy"
-export ignore_templates=""
--- openshift/settings.prime.sh
+++
@@ -1,4 +0,0 @@
-# Description: Prime Health profile
-export SKIP_PIPELINE_PROCESSING=1
-export include_templates="issuer-agent-deploy issuer-wallet-deploy visual-verifier-deploy"
-export ignore_templates=""
