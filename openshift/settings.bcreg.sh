78090368d008c62b341f57399cfcd810d397bb45
Emiliano Suñé <emiliano.sune@gmail.com>
2024-10-29 16:18:30 -0700

    Remove old unused openshift profiles and resources
    Signed-off-by: Emiliano Suñé <emiliano.sune@gmail.com>

--- openshift/settings.bcreg.sh
+++
@@ -1,4 +0,0 @@
-# Description: BC Registries profile
-export SKIP_PIPELINE_PROCESSING=1
-export include_templates="issuer-agent-deploy issuer-wallet-deploy"
-export ignore_templates=""
--- openshift/settings.rev.sh
+++
@@ -1,4 +0,0 @@
-# Description: vc-authn-tmp controller/agent - required until revocation is sorted out
-export SKIP_PIPELINE_PROCESSING=1
-export include_templates="controller-rev-build controller-rev-deploy agent-rev-deploy"
-export ignore_templates=""
